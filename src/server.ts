268f473151a628a25599b8c3801fc40057648e98
danielnevesfonseca <daniel.neves.fonseca.ti@gmail.com>
2024-01-13 15:02:22 -0300

    feat: add deploy config to render.com

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ const PORT = 3333
 
 app.listen(PORT, () => {
   console.log(`Server Running at port ${PORT}`)
-  console.log(`To access the documentation go to http://localhost:${PORT}/api-documentation`)
+  console.log(`To access the documentation go to https://pet-info-api-5kzz.onrender.com/api-documentation`)
 })
